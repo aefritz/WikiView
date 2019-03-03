74b4380cd9d42bf7d66f1127069d03fd8b42d8cd
Austin Fritz <austin.fritz33@gmail.com>
2019-03-03 12:10:03 -0500

    updated header text

--- WikiProj/src/Components/Header.jsx
+++ WikiProj/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header (props) {
   return (<div className="header">
     <h1>WikiView</h1>
-    <h5>Visualizing topics using Wikipedia</h5>
+    <h5>Visualizing information using Wikipedia</h5>
   </div>)
 }
 
